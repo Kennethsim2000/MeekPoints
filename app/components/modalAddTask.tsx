80fdb3fb0d9ced4c27c18c478bc77d9d58fa2d8c
kennethsim2000 <e0725254@u.nus.edu>
2024-08-11 12:03:01 +0800

    Fix selection bug

--- app/components/modalAddTask.tsx
+++ app/components/modalAddTask.tsx
@@ -89,11 +89,12 @@ export default function ModalAddTaskComponent(props: PropType) {
                 aria-label="Default select example"
                 onChange={handleMeekPointsChange}
               >
+                <option value="0">Please select your MeekPoints</option>
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
-                <option value="3">4</option>
-                <option value="3">5</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
               </Form.Select>
             </Form.Group>
           </Form>
